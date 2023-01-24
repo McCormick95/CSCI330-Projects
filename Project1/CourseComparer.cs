6a039b6fa3d8e8f16c9d705e634c6fb2b721c47b
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-01-24 22:22:18 +0000

    Sort by Credits works, need to fix second sort.

--- Project1/CourseComparer.cs
+++ Project1/CourseComparer.cs
@@ -2,34 +2,9 @@ namespace School
 {
     public class CourseComparer : IComparer<Course>
     {
-        public enum SortBy
+        public int Compare(Course x, Course y)
         {
-            Name,
-            Credits
-        }
-        private SortBy compareField = SortBy.Name;
-        public int Compare(Course? x, Course? y)
-        {
-            if(x.Name.CompareTo(y.Name)!= 0)
-            {
-                return x.Name.CompareTo(y.Name);
-            }
-            else if(x.Title.CompareTo(y.Title)!= 0)
-            {
-                return x.Title.CompareTo(y.Title);
-            }
-            else if(x.Credits.CompareTo(y.Credits)!= 0)
-            {
-                return x.Credits.CompareTo(y.Credits);
-            }
-            else if(x.Description.CompareTo(y.Description)!= 0)
-            {
-                return x.Description.CompareTo(y.Description);
-            }
-            else
-            {
-                return 0;
-            }
+            return x.Credits-y.Credits;
         }
     }
 }
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -18,14 +18,58 @@ namespace School
                 Description="Topics include requirements engineering, unite testting, etc...", 
             };
 
+            Course two = new Course(){
+                Name = "CSCI-270",
+                Title= "Data Communications Systems and Networks",
+                Credits= 6,
+                Description="Fundamentals of data communications, including hardware, basic components of communications, etc...", 
+            };
+
+            Course three = new Course(){
+                Name = "MATH-161",
+                Title= "Calculus II",
+                Credits= 4,
+                Description="Topics include math, etc...", 
+            };
+
+            Course four = new Course(){
+                Name = "CSCI-120",
+                Title= "Intro to Web Interface Developent",
+                Credits= 3,
+                Description="An introduction to the design and development of interactive Web 2.0 user interfaces using client-side programming languages.", 
+            };
+
+            Course five = new Course(){
+                Name = "MATH-174",
+                Title= "Introduction to Discrete Mathmatics",
+                Credits= 5,
+                Description="Topics include math, etc...", 
+            };
+
+            Course six = new Course(){
+                Name = "CSCI-356",
+                Title= "Operating Systems",
+                Credits= 3,
+                Description="Basic concepts and terminology of operating systems, I/0 and interrupt structures, etc...", 
+            };
+
             //Console.WriteLine(one);
 
             List<Course> courses = new List<Course>(){
-                one
+                one, two, three, four, five, six
             };
 
-            
+            //courses.Sort();
+            //foreach (Course c in courses){
+            //    Console.WriteLine(c);
+            //}
 
+             Console.WriteLine("-------------------------------------------");
+
+            courses.Sort(new CourseComparer());
+            foreach (Course c in courses){
+                Console.WriteLine(c);
+            }
         }
     }
 
